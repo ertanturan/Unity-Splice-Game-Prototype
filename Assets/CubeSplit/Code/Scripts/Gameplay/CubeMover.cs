3848b145f1f1f2e5a4ad09d822f7a0b88253c0c4
Ertan Turan <ertanturan@outlook.com>
2020-09-26 01:37:16 +0300

    basic movement

---
+++ Assets/CubeSplit/Code/Scripts/Gameplay/CubeMover.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CubeMover : MonoBehaviour
+{
+
+    public Axes Axis;
+    [SerializeField] private float _moveSpeed = 2f;
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float coef = Time.deltaTime * _moveSpeed;
+        Vector3 currentPos = transform.position;
+        switch (Axis)
+        {
+            case Axes.X:
+                currentPos.x += coef;
+                transform.position = currentPos;
+                break;
+
+            case Axes.Y:
+                currentPos.x += coef;
+                transform.position = currentPos;
+                break;
+
+            case Axes.Z:
+                currentPos.x += coef;
+                transform.position = currentPos;
+                break;
+        }
+    }
+}
