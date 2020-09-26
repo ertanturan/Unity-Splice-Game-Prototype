98db30acb50584486922b1485cc859755f42f140
Ertan Turan <ertanturan@outlook.com>
2020-09-26 08:51:44 +0300

    commentary removal

--- Assets/CubeSplit/Code/Scripts/Gameplay/Player.cs
+++ Assets/CubeSplit/Code/Scripts/Gameplay/Player.cs
@@ -36,7 +36,7 @@ public class Player : SceneSingleton<Player>
 
 
         float hangOver = transform.position.x - blade.transform.position.x;
-        hangOver = 1 - hangOver;
+        hangOver = _initialScale - hangOver;
         Debug.Log(hangOver);
         SplitCubeOnX(blade, hangOver);
 
@@ -48,32 +48,6 @@ public class Player : SceneSingleton<Player>
     private void SplitCubeOnX(Blade blade, float hangOver)
     {
 
-        //float newXSize = (transform.localScale.x - blade.transform.localScale.x) - Mathf.Abs(hangOver);
-        //float fallingBlockSize = transform.localScale.x - newXSize;
-
-        //float newXPosition = blade.transform.position.x + (hangOver / 2);
-
-        //if (newXSize < 0)
-        //{
-        //    Debug.LogWarning("failed");
-        //}
-        //transform.localScale = new Vector3(newXSize, transform.localScale.y, transform.localScale.z);
-        //float posDiff = transform.position.x - newXPosition;
-
-        //Debug.Log(transform.position.x);
-        //transform.position = new Vector3(transform.position.x + 2 * Mathf.Abs(posDiff), transform.position.y, transform.position.z);
-
-        //Debug.Log(transform.position.x);/\
-
-
-        //float cubeEdge = transform.position.x + (newXSize / 2) * dir;
-
-        //float fallingBlockXPosition = cubeEdge + fallingBlockSize / 2 * dir;
-
-        //Debug.Log(hangOver);
-        //Debug.Log(dir);
-
-        //SpawnDroppingCube(fallingBlockXPosition, fallingBlockSize);
 
 
 
@@ -83,7 +57,7 @@ public class Player : SceneSingleton<Player>
 
         float cubeEdge = transform.position.x + (newXSize / 2) * -1;
 
-        float newXPosition = blade.transform.position.x + hangOver;
+        float newXPosition = blade.transform.position.x + 2 * hangOver;
 
         transform.localScale = new Vector3(newXSize, transform.localScale.y, transform.localScale.z);
         transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
@@ -103,28 +77,10 @@ public class Player : SceneSingleton<Player>
     private void SpawnDroppingCube(float fallingCubePos, float fallingCubeSize, float hangOver)
     {
 
-        //Vector3 cubeScale = new Vector3(size, transform.localScale.y, transform.localScale.z);
-        //Vector3 cubePos = new Vector3(xPos, transform.position.y, transform.position.z);
 
-
-
-        //GameObject cube =
-        //    ObjectPooler.Instance.SpawnFromPool(PooledObjectType.DroppedCube, cubePos, Quaternion.identity);
-        //cube.transform.localScale = cubeScale;
-        //Debug.Log(cubeScale);
-        //Rigidbody rb = cube.GetComponent<Rigidbody>();
-        //rb.useGravity = true;
-        //rb.isKinematic = false;
-
-        //rb.AddForce(cube.transform.right * -1 * 100);
-
-        //cube.GetComponent<Renderer>().material.color = GetComponent<Renderer>().material.color;
-
-
-        //Destroy(cube.gameObject, 1f);
 
         Vector3 cubeScale = new Vector3(fallingCubeSize, transform.localScale.y, transform.localScale.z);
-        Vector3 cubePos = new Vector3(fallingCubePos - hangOver, transform.position.y, transform.position.z);
+        Vector3 cubePos = new Vector3(fallingCubePos - 2 * hangOver, transform.position.y, transform.position.z);
 
 
         GameObject cube = ObjectPooler.Instance.SpawnFromPool(PooledObjectType.DroppedCube, cubePos, Quaternion.identity);
