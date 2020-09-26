1f979315bf7f9e1e1b5886d6aab132ccb2dca0fc
Ertan Turan <ertanturan@outlook.com>
2020-09-26 07:57:39 +0300

    dropped cube side bugfix

--- Assets/CubeSplit/Code/Scripts/Gameplay/Player.cs
+++ Assets/CubeSplit/Code/Scripts/Gameplay/Player.cs
@@ -2,18 +2,37 @@
 
 public class Player : SceneSingleton<Player>
 {
+
+    private float _halfScale;
+
+    private void Awake()
+    {
+        _halfScale = transform.localScale.x / 2;
+    }
+
     public void Hit(Blade blade)
     {
-        float hangOver = transform.position.x - blade.transform.position.x;
-
-        float dir = hangOver < 0 ? 1 : -1;
 
 
+        float hangOver = blade.transform.position.x - transform.position.x;
+
+        if (hangOver > 0)
+        {
+            hangOver = Mathf.Clamp(hangOver, 0, _halfScale);
+            hangOver = _halfScale - hangOver;
+        }
+        else
+        {
+            hangOver = Mathf.Clamp(hangOver, -_halfScale, 0);
+            hangOver = _halfScale - hangOver;
+        }
+
+        hangOver -= 1;
+
+
+        float dir = hangOver < 0 ? -1 : 1;
+        //float dir = 1f;
         SplitCubeOnX(blade, hangOver, dir);
-        //transform.localScale = new Vector3(1-hangOver,);
-        //ObjectPooler.Instance.SpawnFromPool(PooledObjectType.DroppedCube, targetPos, Quaternion.identity);
-
-
 
 
 
@@ -23,21 +42,31 @@ public class Player : SceneSingleton<Player>
 
     private void SplitCubeOnX(Blade blade, float hangOver, float dir)
     {
-        float newXSize = (transform.localScale.x - blade.transform.localScale.x) - Mathf.Abs((hangOver));
-        float fallingBlockSize = transform.localScale.x - newXSize;
 
+        float newXSize = (transform.localScale.x - blade.transform.localScale.x) - Mathf.Abs(hangOver);
+        float fallingBlockSize = transform.localScale.x - newXSize;
 
         float newXPosition = blade.transform.position.x + (hangOver / 2);
 
-
+        if (newXSize < 0)
+        {
+            Debug.LogWarning("failed");
+        }
         transform.localScale = new Vector3(newXSize, transform.localScale.y, transform.localScale.z);
-        transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+        float posDiff = transform.position.x - newXPosition;
 
+        Debug.Log(transform.position.x);
+        transform.position = new Vector3(transform.position.x + 2 * Mathf.Abs(posDiff), transform.position.y, transform.position.z);
+
+        Debug.Log(transform.position.x);
 
         float cubeEdge = transform.position.x + (newXSize / 2) * dir;
 
         float fallingBlockXPosition = cubeEdge + fallingBlockSize / 2 * dir;
-        Debug.Log(fallingBlockSize);
+
+        Debug.Log(hangOver);
+        Debug.Log(dir);
+
         SpawnDroppingCube(fallingBlockXPosition, fallingBlockSize);
     }
 
@@ -55,9 +84,11 @@ public class Player : SceneSingleton<Player>
             ObjectPooler.Instance.SpawnFromPool(PooledObjectType.DroppedCube, cubePos, Quaternion.identity);
         cube.transform.localScale = cubeScale;
         Debug.Log(cubeScale);
-        Rigidbody rb = cube.AddComponent<Rigidbody>();
+        Rigidbody rb = cube.GetComponent<Rigidbody>();
         rb.useGravity = true;
         rb.isKinematic = false;
+
+        rb.AddForce(cube.transform.right * -1 * 100);
 
         //cube.GetComponent<Renderer>().material.color = GetComponent<Renderer>().material.color;
 
---
+++ Assets/CubeSplit/Code/Scripts/Managers/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.SceneManagement;
+
+public class GameManager : SceneSingleton<GameManager>
+{
+
+    //public bool IsFailed(float hangover, Blade blade)
+    //{
+    //    return true;
+    //    //if (Mathf.Abs(hangover) >= blade.transform.localScale.x)
+    //    //{
+    //    //    //Reload();
+    //    //    return true;
+    //    //}
+
+    //    //return false;
+    //}
+
+    private void Reload()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
